93823ffcaf5f4e4eeaf06c4283db849cd921a93f
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-04-30 23:01:54 +0300

    Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,12 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   ngOnInit(): void {
-    const observable = new Observable<number>(data => {
-      data.next(3);
-      data.next(5);
-      data.next(10);
-      data.complete();
-    });
+    // const observable = new Observable<number>(data => {
+    //   data.next(3);
+    //   data.next(5);
+    //   data.next(10);
+    //   data.complete();
+    // });
 
     // const observer = function (data: any) {
     //   console.log(data);
@@ -26,8 +26,52 @@ export class AppComponent implements OnInit{
     //   console.log(data);
     // }
 
-    observable.subscribe(data => {
-      console.log(data);
-    });
+    // observable.subscribe(data => {
+    //   console.log(data);
+    // });
+
+    // subject
+
+    // const subject = new Subject();
+    // subject.subscribe(data => {console.log(`ObserverA ${data}`)});
+    // subject.subscribe(data => {console.log(`ObserverB ${data}`)});
+    // subject.next(3);
+    // subject.next(4);
+    // subject.subscribe(data => {console.log(`ObserverC ${data}`)});
+    // subject.next(5);
+    // subject.next(6);
+    // subject.next("Ahmet");
+
+    // let data : any = "Hilmi";
+    // const subject = new BehaviorSubject(data);
+    // subject.subscribe(data => {console.log(`ObserverA ${data}`)});
+    // subject.subscribe(data => {console.log(`ObserverB ${data}`)});
+    // subject.next(3);
+    // subject.next(4);
+    // subject.subscribe(data => {console.log(`ObserverC ${data}`)});
+    // subject.next(5);
+    // subject.next(6);
+    // subject.next("Ahmet");
+
+    // const subject = new ReplaySubject(2);
+    // subject.subscribe(data => {console.log(`ObserverA ${data}`)});
+    // subject.subscribe(data => {console.log(`ObserverB ${data}`)});
+    // subject.next(3);
+    // subject.next(4);
+    // subject.subscribe(data => {console.log(`ObserverC ${data}`)});
+    // subject.next(5);
+    // subject.next(6);
+    // subject.next("Ahmet");
+
+    const subject = new AsyncSubject();
+    subject.subscribe(data => {console.log(`ObserverA ${data}`)});
+    subject.subscribe(data => {console.log(`ObserverB ${data}`)});
+    subject.next(3);
+    subject.next(4);
+    subject.subscribe(data => {console.log(`ObserverC ${data}`)});
+    subject.next(5);
+    subject.next(6);
+    subject.complete();
+    subject.next("Ahmet");
   }
 }
