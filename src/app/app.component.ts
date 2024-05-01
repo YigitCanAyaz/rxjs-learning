097b22cb740e1b04468f39adf98064d85eb20d87
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-05-01 19:11:08 +0300

    operators - ajax - bindCallback - defer - empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject, asyncScheduler, observeOn } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject, asyncScheduler, bindCallback, defer, empty, observeOn, of, timer } from 'rxjs';
+import {ajax} from "rxjs/ajax"
+
+declare var $ : any;
 
 @Component({
   selector: 'app-root',
@@ -106,6 +109,48 @@ export class AppComponent implements OnInit, AfterViewInit{
     // console.log("Scheduler kullanılan ****");
 
 
-    console.log("1");
+    // console.log("1");
+
+    // operators - ajax
+    
+    // ajax.getJSON("https://jsonplaceholder.typicode.com/posts")
+    // .subscribe(data => console.log(data));
+
+
+    // ajax("https://jsonplaceholder.typicode.com/posts")
+    // .subscribe(data => console.log(data));
+
+    // ajax({
+    //   url: "....",
+    //   method: "post"
+    // })
+
+    // operators - bindCallback 
+
+    // $.getJSON("https://jsonplaceholder.typicode.com/posts", (data: any) => {
+    //   console.log(data);
+    // });
+
+    // const obs : (url: string) => Observable<any[]> = bindCallback($.getJSON);
+
+    // obs("https://jsonplaceholder.typicode.com/posts")
+    // .subscribe(data => {console.log(data)});
+
+    // operators - defer
+
+    // const obs1 = of(new Date());
+
+    // // subscribe olana kadar oluşturulması ertelenecektir.
+    // const obs2 = defer(() => of(new Date()));
+
+    // timer(5000).subscribe(() => {
+    //   obs1.subscribe(data => console.log(data));
+    //   obs2.subscribe(data => console.log(data));
+    // });
+
+    // operator - empty
+
+    const result = empty();
+    result.subscribe(data => console.log(data));
   }
 }
